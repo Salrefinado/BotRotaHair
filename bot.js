02fbac31f01f85f1a4ac2cfd21650fc6332f47b0
Salrefinado <erickestefanskiabc@gmail.com>
2026-04-11 23:58:31 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,10 +48,7 @@ const client = new Client({
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
       '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process',
-      '--disable-gpu',
+      '--disable-gpu'
     ],
   },
 });
